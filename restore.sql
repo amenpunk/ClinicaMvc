2afac4f53bee83754b61f10256d03320616f7b2c
amenpunk <ondasycircuitos@gmail.com>
2019-09-08 13:32:50 -0600

    sql model :package:

--- restore.sql
+++ restore.sql
@@ -11,7 +11,6 @@ create table admin_login(
 
 
 create table paciente(
-
     id_paciente int IDENTITY (1,1)  not null,
     primer_nombre varchar(50),
     segundo_nombre varchar(50),
@@ -43,23 +42,13 @@ create table cita(
     CONSTRAINT fk_pa FOREIGN KEY(id_paciente) REFERENCES paciente(id_paciente)
 )
 
-create table expediente(
-    id_expediente int IDENTITY(1,1) not null,
-    fecha_gen date,
-    id_paciente int,
-    CONSTRAINT pk_exp PRIMARY KEY(id_expediente),
-    CONSTRAINT fk_pac FOREIGN KEY(id_paciente) REFERENCES paciente(id_paciente)
-)
-
-
 create table doctor(
     id_doctor int IDENTITY(1,1) not null,
-    no_colegiado int,
+    no_colegiado varchar(250),
     nombre varchar(350),
 
     CONSTRAINT pk_doc PRIMARY KEY(id_doctor)
 )
-
 
 CREATE table enfermera(
     id_enfermera int IDENTITY(1,1) not null,
@@ -67,6 +56,14 @@ CREATE table enfermera(
     estado char(1),
 
     CONSTRAINT pk_enf PRIMARY KEY(id_enfermera)
+)
+
+create table expediente(
+    id_expediente int IDENTITY(1,1) not null,
+    fecha_gen date,
+    id_paciente int,
+    CONSTRAINT pk_exp PRIMARY KEY(id_expediente),
+    CONSTRAINT fk_pac FOREIGN KEY(id_paciente) REFERENCES paciente(id_paciente)
 )
 
 create table signos(
@@ -80,25 +77,56 @@ create table signos(
     frec_respiratoria FLOAT,
     id_enfermera int,
     fecha date
+    id_consulta int,
     
     CONSTRAINT pk_sig PRIMARY KEY(id_medicion),
     CONSTRAINT fk_enf FOREIGN KEY(id_enfermera) REFERENCES enfermera(id_enfermera)
+    CONSTRAINT fk_con FOREIGN KEY(id_consulta) REFERENCES consulta(id_consulta)
+)
+
+
+create table expediente(
+    id_expediente int IDENTITY(1,1) not null,
+    fecha_gen date,
+    id_paciente int,
+    CONSTRAINT pk_exp PRIMARY KEY(id_expediente),
+    CONSTRAINT fk_pac FOREIGN KEY(id_paciente) REFERENCES paciente(id_paciente)
 )
 
 CREATE table consulta(
     id_consulta int IDENTITY(1,1),
-    id_medicion int,
     id_doctor int,
     asunto varchar(450),
     monto_caja FLOAT,
     fecha datetime,
     tipo_consulta varchar(100),
     id_expediente int,
+    seguro_medico int,
+    nombre_compania varchar(500),
+    poliza_seguro varchar(250),
+    relacion varchar(50),
 
     CONSTRAINT pk_con PRIMARY KEY(id_consulta),
-    CONSTRAINT fk_sig FOREIGN KEY(id_medicion)  REFERENCES signos(id_medicion),
     CONSTRAINT fk_doc FOREIGN KEY(id_doctor) REFERENCES doctor(id_doctor),
     CONSTRAINT fk_exp FOREIGN KEY(id_expediente) REFERENCES expediente(id_expediente)
+)
+
+create table signos(
+    id_medicion int IDENTITY(1,1) not null, 
+    estatura FLOAT,
+    peso FLOAT,
+    temp FLOAT,
+    pulso FLOAT,
+    presion_art FLOAT,
+    frec_cardiaca FLOAT,
+    frec_respiratoria FLOAT,
+    id_enfermera int,
+    fecha date,
+    id_consulta int,
+
+    CONSTRAINT pk_sig PRIMARY KEY(id_medicion),
+    CONSTRAINT fk_con FOREIGN KEY(id_consulta) REFERENCES consulta(id_consulta),
+    CONSTRAINT fk_enf FOREIGN KEY(id_enfermera) REFERENCES enfermera(id_enfermera)
 )
 
 
@@ -108,7 +136,7 @@ CREATE TABLE diagnostico(
     id_consulta int,
     
     CONSTRAINT pk_diag PRIMARY KEY(id_diagnostico),
-    CONSTRAINT fk_con FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta)
+    CONSTRAINT fk_con FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta),
 )
 
 create table orden_lab(
@@ -125,42 +153,35 @@ create table factura(
     id_expediente int,
     
     CONSTRAINT pk_num PRIMARY KEY(num_factura),
-    CONSTRAINT fk_exp FOREIGN KEY(id_expediente) REFERENCES expediente(id_expediente)
+    CONSTRAINT fk_expd FOREIGN KEY(id_expediente) REFERENCES expediente(id_expediente),
 )
 
 create table detalle_fac(
     num_detalle int IDENTITY(1,1) not null,
     num_factura int,
-    id_producto int,
+    nombre_consulta varchar(250),
     cantidad int,
     precio FLOAT
 
     CONSTRAINT pk_det PRIMARY KEY(num_detalle),
-    CONSTRAINT fk_num FOREIGN KEY(num_factura) REFERENCES factura(num_factura)
+    CONSTRAINT fk_num FOREIGN KEY(num_factura) REFERENCES factura(num_factura),
 )
 
 CREATE table receta(
     id_receta int IDENTITY(1,1) not null,
     id_consulta int,
 
-    CONSTRAINT pk_res PRIMARY KEY(id_receta),
-    CONSTRAINT fk_cons FOREIGN KEY(id_consulta) REFERENCES consulta(id_consulta),
+    CONSTRAINT pk_rese PRIMARY KEY(id_receta),
+    CONSTRAINT fk_consulta FOREIGN KEY(id_consulta) REFERENCES consulta(id_consulta),
 )
 
 create table des_receta(
     id_descripcion int IDENTITY(1,1) not null,
     id_receta int,
-    id_medicamento int,
+    medicamento varchar(250),
     cantidad float,
     dosis varchar(250),
 
     CONSTRAINT pk_des PRIMARY KEY(id_descripcion),
-    CONSTRAINT fk_rec FOREIGN KEY(id_receta) REFERENCES receta(id_receta),
-    CONSTRAINT fk_med FOREIGN KEY(id_medicamento) REFERENCES id_medicamento(id_medicamento),
-)
-
-create table id_medicamento(
-    id_medicamento int IDENTITY(1,1) not null,
-    nombre varchar(250),
-    CONSTRAINT pk_med PRIMARY KEY(id_medicamento)    
+    CONSTRAINT fk_reset FOREIGN KEY(id_receta) REFERENCES receta(id_receta),
 )
