086a44e9f3086e9d3eed2a7040c8587fe829ab4d
amenpunk <ondasycircuitos@gmail.com>
2019-11-01 13:20:12 -0600

    all post

--- wwwroot/js/post.js
+++ wwwroot/js/post.js
@@ -1,5 +1,29 @@
-$("button").click(function(){
-    console.log('click en un boton');
+$('button').click(function(){
+	var action = this.id;
+	var titulo = document.title;
+	var nombre = $('#userid').text()
+	var user = 5190161514;
+	var datos = JSON.stringify({
+		"app":user,
+		"usuario":nombre,
+		"pagename":titulo,
+		"actionname":action
+	})
+	
+	$.ajax({
+		type: "POST",
+		url: "https://us-east-1.aws.webhooks.mongodb-stitch.com/api/client/v2.0/app/hackathon-gmoid/service/API/incoming_webhook/analitica",
+		data: datos,
+		contentType: 'application/json',
+	}).done(function (result) {
+		if (result != null) {
+			console.log("POST REALIZADO")
+		}
+		else {
+			console.log("Algo Salio Mal")
+		}
+	}).fail(function (xhr, status, error) { })
+	
 });
 
 
@@ -14,8 +38,6 @@ $( document ).ready(function() {
 		"pagename":titulo
 	})
 
-	console.table(datos)
-	/*
 	$.ajax({
 		type: "POST",
 		url: "https://us-east-1.aws.webhooks.mongodb-stitch.com/api/client/v2.0/app/hackathon-gmoid/service/API/incoming_webhook/analitica",
@@ -29,5 +51,33 @@ $( document ).ready(function() {
 			console.log("Algo Salio Mal")
 		}
 	}).fail(function (xhr, status, error) { })
-	*/
+});
+
+
+$('a').click(function(){
+	var action = $(this).text();
+	var destiny = $(this).attr('href');
+	var nombre = $('#userid').text()
+	var user = 5190161514;
+	var datos = JSON.stringify({
+		"app":user,
+		"usuario":nombre,
+		"actionname":action,
+		"destiny" : destiny
+	})
+	
+	$.ajax({
+		type: "POST",
+		url: "https://us-east-1.aws.webhooks.mongodb-stitch.com/api/client/v2.0/app/hackathon-gmoid/service/API/incoming_webhook/analitica",
+		data: datos,
+		contentType: 'application/json',
+	}).done(function (result) {
+		if (result != null) {
+			console.log("POST REALIZADO")
+		}
+		else {
+			console.log("Algo Salio Mal")
+		}
+	}).fail(function (xhr, status, error) { })
+	
 });
