51d0207db4855cc59afddd90c6af31148e781d46
amenpunk <ondasycircuitos@gmail.com>
2019-11-01 12:22:49 -0600

    webpost :rocket:

--- wwwroot/js/post.js
+++ wwwroot/js/post.js
@@ -4,6 +4,30 @@ $("button").click(function(){
 
 
 $( document ).ready(function() {
-    var titulo = document.title;
-    console.log(titulo)
+	var titulo = document.title;
+	var nombre = $('#userid').text()
+	
+	var user = 5190161514;
+	var datos = JSON.stringify({
+		"app":user,
+		"usuario":nombre,
+		"pagename":titulo
+	})
+
+	console.table(datos)
+	/*
+	$.ajax({
+		type: "POST",
+		url: "https://us-east-1.aws.webhooks.mongodb-stitch.com/api/client/v2.0/app/hackathon-gmoid/service/API/incoming_webhook/analitica",
+		data: datos,
+		contentType: 'application/json',
+	}).done(function (result) {
+		if (result != null) {
+			console.log("POST REALIZADO")
+		}
+		else {
+			console.log("Algo Salio Mal")
+		}
+	}).fail(function (xhr, status, error) { })
+	*/
 });
